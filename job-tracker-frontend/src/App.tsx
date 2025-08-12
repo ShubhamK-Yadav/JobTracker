4a18f75b21ffba19f18a4d177645462fe2f9977e
ShubhamK-Yadav <syadav2098@gmail.com>
2025-08-12 21:34:56 +0100

    style: Added tailwindcss to the project.

--- job-tracker-frontend/src/App.tsx
+++ job-tracker-frontend/src/App.tsx
@@ -2,12 +2,14 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Dashboard from './dashboard/dashboard'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
+      <Dashboard></Dashboard>
       <div>
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
---
+++ job-tracker-frontend/src/dashboard/dashboard.tsx
@@ -0,0 +1,10 @@
+
+function Dashboard() {
+    return (
+        <div className="bg-white dark:bg-gray-800">
+            <h2> Dashboard </h2>
+        </div>
+    )
+}
+
+export default Dashboard
