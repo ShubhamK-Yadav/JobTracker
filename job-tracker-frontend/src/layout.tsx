e71317e953677e7c77f5bd3e5e2789b21ebf700d
Shubham Yadav <syadav2098@gmail.com>
2025-10-08 23:26:23 +0100

    style: changed the layout slightly.
    - Slight tweak to the layout, will require more tweaks later.

--- job-tracker-frontend/src/layout.tsx
+++ job-tracker-frontend/src/layout.tsx
@@ -1,13 +1,12 @@
 //TODO: Change this to layout.tsx
 import {Outlet} from "react-router";
 import NavBar from "./components/NavBar";
-// import Sidebar from "./components/SideBar";
 import "./index.css";
 
 export default function Layout() {
     return (
         <>
-            <div className="flex h-screen bg-gray-100 dark:bg-gray-900">
+            <div className="flex h-screen bg-slate-400">
                <main className="flex-1 overflow-y-auto">
                     <NavBar/>
                     <Outlet />
